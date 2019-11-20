7b1005baf05669e6d6e6aa84150c66ff532c9f0e
dasung jung <54258507+rhakdkdl16@users.noreply.github.com>
2019-11-20 16:39:53 +0900

    d
    d

---
+++ Assets/ScrollObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollObject : MonoBehaviour
+{
+    [SerializeField] float startPosition;
+    [SerializeField] float endPosition;
+    [SerializeField] float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-1*speed*Time.deltaTime,0,0);
+        if(transform.localPosition.x <= endPosition)
+        {
+            transform.localPosition = new Vector2(startPosition,transform.localPosition.y);
+        }
+        
+    }
+}
